ce1d3fbdc3ba1788fcba134e436f926a88905ad5
syal khan <sayalafridai@gmail.com>
2022-10-10 16:42:50 +0500

    Add files via upload

--- task20.js
+++ task20.js
@@ -1,16 +1,13 @@
-// var mountains = ["K2", "everest","speen ghar","tatara","nanga perbet"];
-// var rivers = ["neil", "sindh", "amazon"];
-// var countries = ["Pakistan", "india", "bangladesh","sarilanka"];
-// var cities = ["islamabad", "riaz", "kashmir", "ziarat"];
-// var languages = ["urdu", "english", "pashto","spanish"];
-// //combination of arrays in single array
-// var nature =[];
-// nature[0]= countries[0];
-// nature[1]= languages[0];
-// nature[2]=rivers[1];
-// nature[3]= cities[3];
-// nature[4]= mountains[2];
-// console.log(nature);
-
-var name1 = "daniyal";
-console.log("first name:", name1);
+var mountains = ["K2", "everest","speen ghar","tatara","nanga perbet"];
+var rivers = ["neil", "sindh", "amazon"];
+var countries = ["Pakistan", "india", "bangladesh","sarilanka"];
+var cities = ["islamabad", "riaz", "kashmir", "ziarat"];
+var languages = ["urdu", "english", "pashto","spanish"];
+//combination of arrays in single array
+var nature =[];
+nature[0]= countries[0];
+nature[1]= languages[0];
+nature[2]=rivers[1];
+nature[3]= cities[3];
+nature[4]= mountains[2];
+console.log(nature);
